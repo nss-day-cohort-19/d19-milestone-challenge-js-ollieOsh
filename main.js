b4c77a3e14641e225e4ea2d5daec15fe63c7bb67
ollieOsh <sosinusi@gmail.com>
2017-04-17 14:36:45 -0500

    i should get better at reading directions

--- main.js
+++ main.js
@@ -1,34 +1,42 @@
 var height = document.getElementById("height"),
 	char = document.getElementById("character"),
-	button = document.getElementById("button");
+	button = document.getElementById("button"),
+	obj = {};
 
-function tree(hgt, chr) {
+function tree(leaf) {
 	var result = "";
 
-	if(!(hgt) || !(chr)) {
+	if(!(leaf.hgt) || !(leaf.chr)) {
 		alert("Both fields must have a value")
 	} else{
-		for(var i = 1;i <= hgt; i++){
+		for(var i = 1;i <= leaf.hgt; i++){
 			var center = " ",
-			length = chr.length;
+			length = leaf.chr.length;
 
-			result += center.repeat(length * (hgt - i)) + chr.repeat((i * 2) - 1) + "\n";
+			result += center.repeat(length * (leaf.hgt - i)) + leaf.chr.repeat((i * 2) - 1) + "\n";
 		}
 		console.log(result);
 	}
 }
 
 button.onclick =  function() {
-	tree(height.value, char.value)
-};
+	obj.hgt = height.value;
+	obj.chr = char.value;
+	tree(obj);
+}
+
 height.onkeydown = function (event){
 	if(event.keyCode == 13) {
-			tree(height.value, char.value);
+		obj.hgt = height.value;
+		obj.chr = char.value;
+		tree(obj);
 	}
 }
 
 char.onkeydown = function (event){
 	if(event.keyCode == 13) {
-			tree(height.value, char.value);
+		obj.hgt = height.value;
+		obj.chr = char.value;
+		tree(obj);
 	}
 }
