829721367dfddc7be77a1ef328458543db282ac8
ollieOsh <sosinusi@gmail.com>
2017-04-17 14:25:18 -0500

    modified tree function to accept arguments of height and character

--- main.js
+++ main.js
@@ -2,34 +2,33 @@ var height = document.getElementById("height"),
 	char = document.getElementById("character"),
 	button = document.getElementById("button");
 
-function tree() {
+function tree(hgt, chr) {
 	var result = "";
 
-	if(!(height.value) || !(char.value)) {
+	if(!(hgt) || !(chr)) {
 		alert("Both fields must have a value")
 	} else{
-		for(var i = 1;i <= height.value; i++){
+		for(var i = 1;i <= hgt; i++){
 			var center = " ",
-			length = char.value.length;
+			length = chr.length;
 
-			result += center.repeat(length * (height.value - i)) + char.value.repeat((i * 2) - 1) + "\n";
+			result += center.repeat(length * (hgt - i)) + chr.repeat((i * 2) - 1) + "\n";
 		}
-		console.log(length);
 		console.log(result);
 	}
 }
 
 button.onclick =  function() {
-	tree()
+	tree(height.value, char.value)
 };
 height.onkeydown = function (event){
 	if(event.keyCode == 13) {
-			tree();
+			tree(height.value, char.value);
 	}
 }
 
 char.onkeydown = function (event){
 	if(event.keyCode == 13) {
-			tree();
+			tree(height.value, char.value);
 	}
 }
