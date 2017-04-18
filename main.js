8352f5abde1af6093712047a95ba54e264dcb618
ollieOsh <sosinusi@gmail.com>
2017-04-18 17:32:44 -0500

    added another solution and printing to DOM

--- main.js
+++ main.js
@@ -4,18 +4,29 @@ var height = document.getElementById("height"),
 	obj = {};
 
 function tree(leaf) {
-	var result = "";
+	var result = "",
+		result2 = "";
 
 	if(!(leaf.hgt) || !(leaf.chr)) {
 		alert("Both fields must have a value")
 	} else{
-		for(var i = 1;i <= leaf.hgt; i++){
-			var center = " ",
-			length = leaf.chr.length;
+		// for(var i = 1;i <= leaf.hgt; i++){
+		//	var center = " ",
+		// 	length = leaf.chr.length;
 
-			result += center.repeat(length * (leaf.hgt - i)) + leaf.chr.repeat((i * 2) - 1) + "\n";
+		// 	result += center.repeat(length * (leaf.hgt - i)) + leaf.chr.repeat((i * 2) - 1) + "\n";
+		// }
+
+		//ANOTHER (a bit simpler) SOLUTION
+		var z = leaf.hgt;
+
+		for(var i = 0; i < leaf.hgt; i++) {
+			result += '&nbsp;&nbsp;'.repeat(z) + leaf.chr.repeat((i * 2) + 1) + '<br>';
+			result2 += ' '.repeat(z) + leaf.chr.repeat((i * 2) + 1) + '\n';
+		 	z--;
 		}
-		console.log(result);
+		console.log(result2);
+		document.getElementById('stuff').innerHTML = result;
 	}
 }
 
